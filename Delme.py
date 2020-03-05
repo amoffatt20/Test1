9ee2cebc39a6dfab4014feeb3fec40950d847ba4
amoffatt20 <44112312+amoffatt20@users.noreply.github.com>
2020-03-04 18:26:20 -0800

    First File
    These are the first files im adding

---
+++ Delme.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 09:07:26 2019
+
+@author: andre
+"""
+import time
+from paraview.simple import *
+print("hello")
+
+x = 0
+while x < 10:
+    vv.hideMeasurementGrid()
+    time.sleep(0.5)
+    vv.showMeasurementGrid()
+    time.sleep(0.5)
+    x+=1
